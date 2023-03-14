7893ae7588e32b27a4b03dc224b93049cb4342f5
Darshana Perera <darshana.p@hsenidmobile.com>
2023-03-14 09:58:23 +0530

    code for app-script code added

---
+++ src/Design/backend/Appscript.js
@@ -0,0 +1,6 @@
+// const sheets = SpreadsheetApp.openByUrl("https://docs.google.com/spreadsheets/d/1bYLP3eAu5k-UT1C4PAHwDKr3_gej_FL-LnUs0pyEt04/edit#gid=0");
+// const sheet = sheets.getSheetByName("Sheet1");
+// function doPost(e) {
+//   var data = JSON.parse(e.postData.contents);
+//   sheet.appendRow([data.fullName, data.contact, data.email, data.company, data.job, data.industry, data.size, data.country, data.answers[0], data.answers[1], data.answers[2], data.answers[3], data.answers[4], data.answers[5], data.answers[6], data.answers[7], data.answers[8], data.answers[9], data.answers[10], data.answers[11], data.answers[12], data.answers[13], data.answers[14], data.answers[15], data.answers[16]]);
+// }
